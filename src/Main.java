547c2869f84cccd89400df68484830c8b8563842
Marcos Vinicius <marcos.vinicius.alves.souza@outlook.com>
2025-12-11 16:40:53 -0300

    Ajustei a Main para me incluir como desenvolvedor e ajustei o bootcamp para o atual que estou fazendo da CI&T.

--- src/Main.java
+++ src/Main.java
@@ -12,37 +12,27 @@ public class Main {
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
-        curso2.setCargaHoraria(4);
-
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        /*System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
-
         Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.setNome("CI&T - Backend com Java & AWS");
+        bootcamp.setDescricao("Domine o desenvolvimento backend profissional com Java e AWS.");
         bootcamp.getConteudos().add(curso1);
-        bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
 
-        Dev devCamila = new Dev();
-        devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        devCamila.progredir();
-        devCamila.progredir();
+        Dev devVinicius = new Dev();
+        devVinicius.setNome("Marcos Vinicius");
+        devVinicius.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Vinicius:" + devVinicius.getConteudosInscritos());
+        devVinicius.progredir();
+        devVinicius.progredir();
         System.out.println("-");
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
-        System.out.println("XP:" + devCamila.calcularTotalXp());
+        System.out.println("Conteúdos Inscritos Vinicius:" + devVinicius.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Vinicius:" + devVinicius.getConteudosConcluidos());
+        System.out.println("XP:" + devVinicius.calcularTotalXp());
 
         System.out.println("-------");
 
